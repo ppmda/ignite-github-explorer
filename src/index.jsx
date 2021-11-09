f80a7f84a5d6c83df00e7fe02feaaad3fd3f69b2
ppmda <pmlclh@gmail.com>
2021-11-08 22:13:38 -0300

    Configuraçao de ambiente, conceitos importantes do react

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+import { App } from "./App";
+
+//na função render, primeiro passa o componente, e depois o objeto com o id
+render(<App />, document.getElementById("root"));
